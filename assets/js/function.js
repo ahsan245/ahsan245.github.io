1e2b88d837908839599d09c4117d7e756cdf6b4e
Muhammad Ahsan <muhammadahsanty@outlook.com>
2023-01-08 00:39:15 +0500

    Added Tech Details and make the Status with API

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,15 +5,12 @@ $(document).ready(function () {
     LoadTechnicians();
 });
 
-function LoadTechnicians()
-{
+function LoadTechnicians() {
     $.ajax({
         type: "GET",
         url: "http://127.0.0.1:4000/api/tech",
-        success: function (data) 
-        {
-            if (data.message == "Success") 
-            {
+        success: function (data) {
+            if (data.message == "Success") {
                 LoadComplains(data.data);
             }
         }
@@ -22,100 +19,120 @@ function LoadTechnicians()
 
 
 
-function LoadComplains(TechData) 
-{
+function LoadComplains(TechData) {
     $.ajax({
         type: "GET",
         url: "http://127.0.0.1:4000/api/complain",
-        success: function (data) 
-        {
-            if (data.messege == "Success") 
-            {
+        success: function (data) {
+            if (data.messege == "Success") {
                 LoadAllComplain(data.data, TechData);
             }
         }
     });
 }
 
-function LoadAllComplain(Complaindata, Techniciandata)
-{
-    debugger;
+function LoadAllComplain(Complaindata, Techniciandata) {
     $("#allComplains tr").remove();
-    $.each(Complaindata, function (k, v) 
-    {
+    $.each(Complaindata, function (k, v) {
         var html = '<tr>';
-        html +='<td>';
-        html +='<div class="d-flex px-2 py-1">';
-        html +='<div>';
-        html +='<img src="assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">'; // baseUrl + v.complainImage
-        html +='</div>';
-        html +='<div class="d-flex flex-column justify-content-center">';
-        html +='<h6 class="mb-0 text-sm">'+v.user.fullName+'</h6>';
-        html +='<p class="text-xs text-secondary mb-0">'+v.user.userId+'</p>';
-        html +='</div>';
-        html +='</div>';
-        html +='</td>';
-        html +='<td>';
-        html +='<p class="text-xs font-weight-bold mb-0">'+v.userAddress+'</p>';
-        html +='<p class="text-xs text-secondary mb-0">'+v.userContact+'</p>';
-        html +='</td>';
-        html +='<td>';
-        html +='<p class="text-xs font-weight-bold mb-0">'+v.complainName+'</p>';
-        html +='<p class="text-xs text-secondary mb-0">'+v.complainCategory+'</p>';
-        html +='<p class="text-xs text-secondary mb-0">'+v.complainId+'</p>';
-        html +='</td>';
-        html +='<td class="align-middle text-center text-sm">';
-        html +='<span class="text-xs font-weight-bold mb-0">'+v.complainDescription+'</span>';
-        html +='</td>';   
-        html +='<td id="technician">';
-        if(v.assignedTech != '' && v.assignedTech != null)
-        {
-            html +='<span class="badge badge-sm bg-gradient-success">'+v.assignedTech+'</span>';
+        html += '<td>';
+        html += '<div class="d-flex px-2 py-1">';
+        html += '<div>';
+        if(v.complainImage == '' || v.complainImage == null){
+            html += '<img src="assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">'; 
+
+            }
+            else{
+                html += '<img src="http://127.0.0.1:4000'+v.complainImage+'" class="avatar avatar-sm me-3" alt="user1">'; // baseUrl + v.complainImage
+
+            }
+        
+        html += '</div>';
+        html += '<div class="d-flex flex-column justify-content-center">';
+        html += '<h6 class="mb-0 text-sm">' + v.user.fullName + '</h6>';
+        html += '<p class="text-xs text-secondary mb-0">' + v.user.userId + '</p>';
+        html += '</div>';
+        html += '</div>';
+        html += '</td>';
+        html += '<td>';
+        html += '<p class="text-xs font-weight-bold mb-0">' + v.userAddress + '</p>';
+        html += '<p class="text-xs text-secondary mb-0">' + v.userContact + '</p>';
+        html += '</td>';
+        html += '<td>';
+        html += '<p class="text-xs font-weight-bold mb-0">' + v.complainName + '</p>';
+        if (v.complainCategory == "" || v.complainCategory == null) {
+            html += '<p class="text-xs text-secondary mb-0"></p>';
+
         }
-        else
+        else {
+            html += '<p class="text-xs text-secondary mb-0">' + v.complainCategory + '</p>';
+        }
+        html += '<p class="text-xs text-secondary mb-0">' + v.complainId + '</p>';
+        html += '</td>';
+        html += '<td class="align-middle text-center text-sm">';
+        html += '<span class="text-xs font-weight-bold mb-0">' + v.complainDescription + '</span>';
+        html += '</td>';
+        html += '<td class="align-middle text-center text-sm">';
+        if(v.complainStatus == true)
         {
-            html += '<select id="allTechnicians-'+ k +'">';
-            $.each(Techniciandata, function (x, y) 
-            {
-                html += '<option value="'+ y.techId +'">'+ y.techName +'</option>';
+        html += '<span class="badge badge-sm bg-gradient-success">Actice</span>';
+        }
+        if(v.complainStatus == false)
+        {
+        html += '<span class="badge badge-sm bg-gradient-warning">InActive</span>';
+
+        }
+
+        html += '</td>';
+        html += '<td id="technician">';
+        if (v.assignedTech != '' && v.assignedTech != null) {
+            html += '<span class="badge badge-sm bg-gradient-faded-info">' + v.assignedTech.techName + '</span>';
+        }
+        else {
+            html += '<select id="allTechnicians-' + k + '" class="form-control">';
+            $.each(Techniciandata, function (x, y) {
+                html += '<option value="' + y.techId + '">' + y.techName + '</option>';
             });
             html += '</select>';
         }
-        html +='</td>';
-        if(v.assignedTech == '' || v.assignedTech == null)
-        {
-            html +='<td class="align-middle">';
-            html +='<a href="javascript:UpdateComplain(\''+ v.complainId+'\','+ k+')" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Assign Technician">';
-            html +='Assign';
-            html +='</a>';
-            html +='</td>';
+        html += '</td>';
+        if (v.assignedTech == '' || v.assignedTech == null) {
+            html += '<td class="align-middle">';
+            html += '<a href="javascript:UpdateComplain(\'' + v.complainId + '\',' + k + ')" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Assign Technician">';
+            html += 'Assign';
+            html += '</a>';
+            html += '</td>';
         }
-        html +='</tr>';
+     
+        
+
+
+
+
+
+        html += '</tr>';
         $("#allComplains").append(html);
     });
 }
 
-function UpdateComplain(complainId, count)
-{
+function UpdateComplain(complainId, count) {
     debugger;
-    var id = document.getElementById("allTechnicians-"+count);
+    var id = document.getElementById("allTechnicians-" + count);
     var technicianId = id.options[id.selectedIndex].value;
     complain = complainId;
     console.log(technicianId);
 
     let formData = new FormData;
-    formData.append('assignedTech',technicianId);
+    formData.append('assignedTech', technicianId);
 
     $.ajax({
         type: "PUT",
-        url: "http://127.0.0.1:4000/api/complain/"+complain,
+        url: "http://127.0.0.1:4000/api/complain/" + complain,
         data: formData,
         contentType: false,
         processData: false,
-        success: function (data) 
-        {
-            if (data.messege == "Success") 
-            {
+        success: function (data) {
+            if (data.messege == "Success") {
                 LoadTechnicians();
             }
         }
---
+++ assets/js/tfunction.js
@@ -0,0 +1,102 @@
+// Project Base URL
+let baseUrl = "127.0.0.1:4000/";
+var complain = "";
+$(document).ready(function () {
+    LoadTechnicians();
+});
+
+function LoadTechnicians() {
+    $.ajax({
+        type: "GET",
+        url: "http://127.0.0.1:4000/api/tech",
+        success: function (data) {
+            if (data.message == "Success") {
+                LoadAllTech(data.data);
+            }
+        }
+    });
+}
+
+
+
+
+function LoadAllTech(Techniciandata) {
+    $("#allComplains tr").remove();
+    $.each(Techniciandata, function (k, v) {
+        var html = '<tr>';
+        html += '<td>';
+        html += '<div class="d-flex px-2 py-1">';
+        html += '<div>';
+        if(v.techImage == '' || v.techImage == null){
+        html += '<img src="assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">'; 
+
+        }
+        else{
+        html += '<img src="http://127.0.0.1:4000'+v.techImage+'" class="avatar avatar-sm me-3" alt="user1">'; // baseUrl + v.complainImage
+        }
+        html += '</div>';
+        html += '<div class="d-flex flex-column justify-content-center">';
+        html += '<h6 class="mb-0 text-sm">' + v.techName + '</h6>';
+        html += '<p class="text-xs text-secondary mb-0">' + v.techId + '</p>';
+        html += '</div>';
+        html += '</div>';
+        html += '</td>';
+        html += '<td>';
+        html += '<p class="text-xs font-weight-bold mb-0">'+'<img src="http://127.0.0.1:4000'+v.category.categoryImage+'" class="avatar avatar-sm me-3" alt="user1"></p>';
+        html += '<p class="text-xs text-secondary mb-0">' + v.category.categoryId + '</p>';
+        html += '</td>';
+        html += '<td>';
+        html += '<p class="text-xs font-weight-bold mb-0">' + v.category.categoryName + '</p>';
+            html += '<p class="text-xs text-secondary mb-0">' + v.techSalePrice + '</p>';
+        html += '<p class="text-xs text-secondary mb-0">' + v.techPrice + '</p>';
+        html += '</td>';
+        html += '<td class="align-middle text-center text-sm">';
+        html += '<span class="text-xs font-weight-bold mb-0">' + v.techShortDescription + '</span>';
+        html += '</td>';
+        html += '<td id="technician">';
+        if(v.techStatus == '' || v.techStatus == null){
+        html += '<span class="badge badge-sm bg-gradient-warning">InActive</span>';
+
+        }
+        if(v.techStatus == false)
+        {
+        html += '<span class="badge badge-sm bg-gradient-success">InActive</span>';
+            
+        }
+        if(v.techStatus == true){
+        html += '<span class="badge badge-sm bg-gradient-success">Active</span>';
+        }
+        html += '</td>';
+            html += '</td>';
+
+
+
+
+        html += '</tr>';
+        $("#allComplains").append(html);
+    });
+}
+
+function UpdateComplain(complainId, count) {
+    debugger;
+    var id = document.getElementById("allTechnicians-" + count);
+    var technicianId = id.options[id.selectedIndex].value;
+    complain = complainId;
+    console.log(technicianId);
+
+    let formData = new FormData;
+    formData.append('assignedTech', technicianId);
+
+    $.ajax({
+        type: "PUT",
+        url: "http://127.0.0.1:4000/api/complain/" + complain,
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            if (data.messege == "Success") {
+                LoadTechnicians();
+            }
+        }
+    });
+}
