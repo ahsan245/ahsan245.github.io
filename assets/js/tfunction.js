88e7531cbebf453964d3b2e3bc716ad388ac0d37
Muhammad Ahsan <muhammadahsanty@outlook.com>
2023-06-03 17:55:34 +0500

    Insert New Tech Login Details

--- assets/js/tfunction.js
+++ assets/js/tfunction.js
@@ -64,10 +64,26 @@ $(document).ready(function () {
     function LoadTechnicians() {
         fetch("https://theek-karo-api.herokuapp.com/api/tech")
             .then(response => response.json())
-            .then(data => {
-                if (data.message === "Success") {
-                    LoadAllTech(data.data);
+            .then(techData => {
+                debugger;
+                if (techData.message === "Success") {
+                    console.log("Tech data message: Success");
+                    fetch("https://theek-karo-api.herokuapp.com/api/findtech")
+                        .then(response => response.json())
+                        .then(findTechData => {
+                            console.log("Find tech data:", findTechData);
+                            if (findTechData.messege === "Success") { // Fix typo here
+                                console.log("Find tech data message: Success");
+                                LoadAllTech(techData.data, findTechData.data);
+                            }
+                        })
+                        .catch(error => {
+                            // Handle the error
+                            console.log("Error: " + error);
+                        });
                 }
+
+
             })
             .catch(error => {
                 // Handle the error
@@ -75,9 +91,37 @@ $(document).ready(function () {
             });
     }
 
-    function LoadAllTech(Techniciandata) {
+
+    function LoadAllTech(techData, findTechData) {
+        console.log("Inside LoadAllTech function");
+        console.log(findTechData);
+
+        const registeredTechIds = findTechData.reduce((ids, item) => {
+            if (item.techID && item.techID.techId) {
+                ids.push(item.techID.techId);
+            }
+            return ids;
+        }, []);
+
+        // ...
+
+        $.each(techData, function (k, v) {
+            // ...
+
+            if (registeredTechIds.includes(v.techId)) {
+                console.log("ID");
+
+            } else {
+                console.log("no ID");
+
+            }
+
+            // ...
+        });
+
+
         $("#allComplains tr").remove();
-        $.each(Techniciandata, function (k, v) {
+        $.each(techData, function (k, v) {
             var html = '<tr>';
             html += '<td>';
             html += '<div class="d-flex px-2 py-1">';
@@ -115,8 +159,72 @@ $(document).ready(function () {
                 html += '<span class="badge badge-sm bg-gradient-success">Active</span>';
             }
             html += '</td>';
+            html += '<td>';
+            if (registeredTechIds.includes(v.techId)) {
+                html += '<h6 class="mb-0"></h6>';
+            } else {
+                html += '<a href="#" class="btn btn-primary btn-sm add-tech-login" data-techid="' + v.techId + '">Register</a>';
+            }
+
+            html += '</td>';
             html += '</tr>';
             $("#allComplains").append(html);
         });
+
+        $(".add-tech-login").click(function () {
+
+            var techId = $(this).data("techid");
+            $("#registerTechId").val(techId);
+            debugger;
+
+            $("#registerTechnicianModal").modal("show");
+
+            $("#registerTechnicianBtn").click(function () {
+
+                var email = $("#registerEmail").val();
+                var password = $("#registerPassword").val();
+
+                // Perform email and password validation using regex patterns
+                var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+                if (!emailRegex.test(email)) {
+                    alert("Please enter a valid email address.");
+                    return;
+                }
+
+                if (!passwordRegex.test(password)) {
+                    alert("Please enter a valid password. It must contain at least 8 characters, including one uppercase letter, one lowercase letter, and one number.");
+                    return;
+                }
+
+                // Send the fetch request to register a technician
+                fetch("https://theek-karo-api.herokuapp.com/api/registerTech", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ techID: techId, email: email, password: password })
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        // Handle the response
+                        if (data.message === "Success") {
+                            // Reload the technicians list
+                            LoadTechnicians();
+                            $("#registerTechnicianModal").modal("hide");
+
+                        } else {
+                            // Handle the error
+                            console.log("Error: " + data.message);
+                        }
+                    })
+                    .catch(error => {
+                        // Handle the error
+                        console.log("Error: " + error);
+                    });
+            });
+        });
+
     }
 });
